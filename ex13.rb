6c8d14ff379ca1fc3e365f90de2ea5bf0c56f696
Vlad Akilov <vakilov@tacitknowledge.com>
2014-05-27 15:32:45 -0400

    Exercise 12, 13

---
+++ ex13.rb
@@ -0,0 +1,6 @@
+first, second, third = ARGV
+
+puts "This script is called #{$0}"
+puts "The first variable is:  #{first}"
+puts "The second variable is:  #{second}"
+puts "The third variable is:  #{third}"
